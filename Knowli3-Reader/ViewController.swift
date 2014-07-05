d892ca5581f903b0a95cfa5ebcc43f5988d02e38
DonghunSeo <sm1835dh@hanmail.net>
2014-07-05 12:12:24 +0900

    Initial Commit

---
+++ Knowli3-Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Knowli3-Reader
+//
+//  Created by 서 동훈 on 2014. 7. 5..
+//  Copyright (c) 2014년 서 동훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
